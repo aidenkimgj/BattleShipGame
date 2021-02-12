e6504001203e5e94a26d1f2296e691eefd4e7ffb
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-11 23:58:13 -0700

    comment

--- src/Game/GameServerThread.java
+++ src/Game/GameServerThread.java
@@ -8,72 +8,75 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * 
+ * @author Hong, Kim, and Sung.
+ * @version Feb 11, 2021
+ */
 class GameServerThread extends Thread {
 
-	// 클라이언트 소켓 저장
-	Socket socket;
+   /**
+    * field - socket, playServer, br, pw, str, name.
+    */
+   Socket socket;
+   PlayServer playServer;
+   private BufferedReader br; 
+   private PrintWriter pw;
+   String str;
+   String name;
 
-	// ChatGUIServer 클래스의 객체를 멤버 변수로 선언, has-a 관계를 위함
-	PlayServer playServer;
+   /**
+    * This is GameServerThread class constructor with playserver and socket parameters.
+    */
+   public GameServerThread(PlayServer playServer, Socket s) {
 
-	private BufferedReader br; 
-	private PrintWriter pw;
+      // save playServer into the field.
+      this.playServer = playServer;
 
-	// 전달할 문자열
-	String str;
+      // save socket into the field.
+      this.socket = s;
 
-	// 대화명(ID)
-	String name;
+      // 데이터 전송을 위한 입출력 스트림 생성
+      /**
+       * create I/O stream for the data transfer.
+       */
+      try {
+         // input - s.getInputStream() => get socker object's inputStream.
+         br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         
+         pw = new PrintWriter(socket.getOutputStream(), true);
 
-	// 생성자
-	public GameServerThread(PlayServer playServer, Socket s) {
-		/*
-		 * cg = new ChatGUIServer(); → 작성 불가, 서버가 두 번 가동되기 때문에 충돌이 일어남 따라서 매개변수를 이용해서
-		 * 객체를 얻어온(call by reference) 뒤에 cg와 s값을 초기화해야 함
-		 */
-		this.playServer = playServer;
+      } catch (Exception e) {
+         System.out.println("Erorr..>>>>>" + e);
+      }
+   }
 
-		// 접속한 클라이언트 정보 저장
-		this.socket = s;
+   // 메세지(입력 문자열) 출력 메소드
+   public void send(String str) {
 
-		// 데이터 전송을 위한 입출력 스트림 생성
-		try {
-			// =========== 입력 ===========
-			// s.getInputStream() => 접속 클라이언트(소켓 객체)의 InputStream을 얻어 옴
-			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			pw = new PrintWriter(socket.getOutputStream(), true);
+      
+      pw.println(str);
+//      pw.flush();
+   }
 
-		} catch (Exception e) {
-			System.out.println("Erorr..>>>>>" + e);
-		}
-	}
+   public void run() {
 
-	// 메세지(입력 문자열) 출력 메소드
-	public void send(String str) {
+      try {
 
-		
-		pw.println(str);
-//		pw.flush();
-	}
+         // 무한 대기하며 입력한 메세지를 각 클라이언트에 계속 전달
+         while ((str = (String) br.readLine()) != null) {
+            playServer.broadCast(str, this);
+            
 
-	public void run() {
+         }
+         
+         
+      } catch (Exception e) {
 
-		try {
+         playServer.removeThread(this); // this: ServerThread 객체, 접속 클라이언트
 
-			// 무한 대기하며 입력한 메세지를 각 클라이언트에 계속 전달
-			while ((str = (String) br.readLine()) != null) {
-				playServer.broadCast(str, this);
-				
-
-			}
-			
-			
-		} catch (Exception e) {
-
-			playServer.removeThread(this); // this: ServerThread 객체, 접속 클라이언트
-
-			System.out.println(socket.getInetAddress() + " connection finished!");
-		}
-	}
+         System.out.println(socket.getInetAddress() + " connection finished!");
+      }
+   }
 
 }
